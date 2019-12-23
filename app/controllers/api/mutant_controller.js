280dfcfd8840f211df0ec15f51e64029ef0d185a
Siuk <cesar.delgado@outlook.com>
2019-12-23 02:43:20 -0300

    Update isMutant

--- app/controllers/api/mutant_controller.js
+++ app/controllers/api/mutant_controller.js
@@ -20,6 +20,10 @@ MutantController.create = function () {
         return isValidateFormat(callback);
       },
       function (resultado, callback) {
+
+        return isMutant(resultado, callback);
+      },
+      function (resultado, callback) {
         // done();
         return res.send(resultado);
       }
@@ -32,15 +36,13 @@ MutantController.create = function () {
   );
 
   function validateOblicuaIzqDer(posIzq, matriz) {
-   /**Función que recorre en forma diagonal de izquierda a derecha la matriz */
+    /**Función que recorre en forma diagonal de izquierda a derecha la matriz */
     let i = 0;
     let j = posIzq;
     let countOblicua = 0;
     let lastOblicua = '';
     let isMutant = false;
     while (i < matriz.length && j < matriz[i].length) {
-      if (AllowedValues.indexOf(matriz[i][j]) == -1)
-        return xmens.wrapper(xmens.constants.BAD_FORMAT_CONTENT);
       if (i == 0)
         lastOblicua = matriz[i][j];
       else {
@@ -50,8 +52,7 @@ MutantController.create = function () {
           lastOblicua = matriz[i][j];
           countOblicua = 0;
         }
-        if (countOblicua == 4)
-        {
+        if (countOblicua == 4) {
           isMutant = xmens.wrapper(xmens.constants.IS_MUTANT);
           break;
         }
@@ -70,8 +71,6 @@ MutantController.create = function () {
     let lastOblicua = '';
     let isMutant = false;
     while (i < matriz.length && j >= 0) {
-      if (AllowedValues.indexOf(matriz[i][j]) == -1)
-        return xmens.wrapper(xmens.constants.BAD_FORMAT_CONTENT);
       if (i == 0)
         lastOblicua = matriz[i][j];
       else {
@@ -81,8 +80,7 @@ MutantController.create = function () {
           lastOblicua = matriz[i][j];
           countOblicua = 0;
         }
-        if (countOblicua == 4)
-        {
+        if (countOblicua == 4) {
           isMutant = xmens.wrapper(xmens.constants.IS_MUTANT);
           break;
         }
@@ -95,21 +93,32 @@ MutantController.create = function () {
 
   function isValidateFormat(cb) {
     let lengthEjeY = pDna.length;
+    for (let i in pDna) {
+      if (lengthEjeY != pDna[i].length)
+        return cb(null, xmens.wrapper(xmens.constants.BAD_MATRIX));
+      for (let j in pDna[i]) {
+        if (AllowedValues.indexOf(pDna[i][j]) == -1)
+          return cb(null, xmens.wrapper(xmens.constants.BAD_FORMAT_CONTENT));
+      }
+    }
+    return cb(null, true);
+  }
+
+  function isMutant(result, cb) {
+    if (result != true)
+      return cb(null, result);
     let lastLetterHorizontal = '';
     let countHorizontal = 0;
     let lastLetterVertical = '';
     let countVertical = 0;
     for (let i in pDna) {
-      if (lengthEjeY != pDna[i].length)
-        return cb(null, xmens.wrapper(xmens.constants.BAD_MATRIX));
       for (let j in pDna[i]) {
-        
         if (i == 0) {
           /**Evalua oblicua de izquierda a derecha */
           let res = validateOblicuaIzqDer(j, pDna);
           if (res != false)
             return cb(null, res);
-           /**Evalua oblicua de derecha a izquierda*/
+          /**Evalua oblicua de derecha a izquierda*/
           res = validateOblicuaDerIzq(j, pDna);
           if (res != false)
             return cb(null, res);
@@ -117,25 +126,21 @@ MutantController.create = function () {
           lastLetterVertical = pDna[i][j];
           let indexY = i;
           while (indexY < pDna.length) {
-            if (AllowedValues.indexOf(pDna[indexY][j]) == -1)
-              return cb(null, xmens.wrapper(xmens.constants.BAD_FORMAT_CONTENT));
             if (lastLetterVertical == pDna[indexY][j])
               countVertical++;
             else {
               lastLetterVertical = pDna[indexY][j];
               countVertical = 0;
             }
-            if (countVertical == 4)
-            {
+            if (countVertical == 4) {
               return cb(null, xmens.wrapper(xmens.constants.IS_MUTANT));
 
             }
-            indexY ++;
+            indexY++;
           }
         }
-        
-        if (j == 0)
-        {
+
+        if (j == 0) {
           /**Evalua oblicua de izquierda a derecha */
           let res = validateOblicuaIzqDer(j, pDna);
           if (res != false)
@@ -154,46 +159,19 @@ MutantController.create = function () {
             return cb(null, xmens.wrapper(xmens.constants.IS_MUTANT));
         }
         /**Evalua oblicua de derecha a izquierda */
-        if (j == pDna[i].length -1)
-        {
+        if (j == pDna[i].length - 1) {
           let res = validateOblicuaDerIzq(j, pDna);
           if (res != false)
             return cb(null, res);
         }
 
       }
-      
+
 
     }
-    return cb(null,  xmens.wrapper(xmens.constants.IS_NOT_MUTANT));
+    return cb(null, xmens.wrapper(xmens.constants.IS_NOT_MUTANT));
   }
 };
 
-// MutantController.before('*', function(next) {
-
-//   var req = this.req,
-//     res = this.res;
-//   return xmens.api.version(req.get('xmens_api_version'), function(info) {
-//     if (info) return res.send(xmens.wrapper(info));
-//     else return xmens.api.auth(req.get('xmens_api_token'), function(err, user, info) {
-//       if (err) {
-//         return res.send(xmens.wrapper(xmens.constants.INTERNAL_ERROR));
-//       }
-//       else if (info) return res.send(xmens.wrapper(info));
-//       else {
-//         req.user = user;
-//         return pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//           if (err) {
-//             done();
-//             return next(err);
-//           }
-//           req.pg_client = client;
-//           req.pg_done = done;
-//           return next();
-//         });
-//       }
-//     });
-//   });
-// });
 
 module.exports = MutantController;
